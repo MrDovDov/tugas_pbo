592ac1411b95f83217541b1b3bfed2f3c6065e64
root <root@LAPTOP-I98TKHLR.localdomain>
2022-10-18 12:04:05 +0700

    tugas4

--- pertemuan4/d.java
+++ pertemuan4/d.java
@@ -40,7 +40,7 @@ public class d {
                 System.out.println("Pesanan : SEAFOOD");
                 System.out.println("jumlah Harga : " + 10000);
                 System.out.println("PPN : " + 10000 * 0.1);
-                System.out.println("TOTAL HARGA : ");
+                System.out.println("TOTAL HARGA : "+ 11000);
                 System.out.println("TOTAL BAYAR : " + 50000);
                 System.out.println("KEMBALIAN : " + 39000);
                 break;
@@ -49,7 +49,7 @@ public class d {
                 System.out.println("Pesanan : NASI GORENG AYAM");
                 System.out.println("jumlah Harga : " + 25000);
                 System.out.println("PPN : " + 25000 * 0.1);
-                System.out.println("TOTAL HARGA : ");
+                System.out.println("TOTAL HARGA : "+ 27500);
                 System.out.println("TOTAL BAYAR : " + 50000);
                 System.out.println("KEMBALIAN : " + 22500);
                 break;
@@ -58,7 +58,7 @@ public class d {
                 System.out.println("Pesanan : NASI GORENG SEAFOOD");
                 System.out.println("jumlah Harga : " + 20000);
                 System.out.println("PPN : " + 20000 * 0.1);
-                System.out.println("TOTAL HARGA : ");
+                System.out.println("TOTAL HARGA : "+ 22000);
                 System.out.println("TOTAL BAYAR : " + 50000);
                 System.out.println("KEMBALIAN : " + 28000);
                 break;
@@ -67,7 +67,7 @@ public class d {
                 System.out.println("Pesanan : MIE GORENG SPESIAL");
                 System.out.println("jumlah Harga : " + 30000);
                 System.out.println("PPN : " + 30000 * 0.1);
-                System.out.println("TOTAL HARGA : ");
+                System.out.println("TOTAL HARGA : "+ 33000);
                 System.out.println("TOTAL BAYAR : " + 50000);
                 System.out.println("KEMBALIAN : " + 17000);
                 break;
